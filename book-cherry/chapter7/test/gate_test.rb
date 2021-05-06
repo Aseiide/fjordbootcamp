5e7395b0080141b42f18a957dc34880cc9fb861e
Aseiide <eiseiide@gmail.com>
2021-05-06 18:34:04 +0900

    fix

--- book-cherry/chapter7/test/gate_test.rb
+++ book-cherry/chapter7/test/gate_test.rb
@@ -3,20 +3,33 @@ require './lib/gate'
 require './lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    kokura = Gate.new(:kokura)
-    nishikokura = Gate.new(:nishikokura)
-    ticket = Ticket.new(170)
-    kokura.enter(ticket)
-    assert nishikokura.exit(ticket)
+  def setup
+    @kokura = Gate.new(:kokura)
+    @nishikokura = Gate.new(:nishikokura)
+    @kyukoudai = Gate.new(:kyukoudai)
   end
 
-  def tetst_kokura_to_nishikokura_when_fare_is_not_enough
-    kokura = Gate.new(:kokura)
-    kyukoudai = Gate.new(:kyukoudai)
-
+  def test_kokura_to_nishikokura
     ticket = Ticket.new(170)
-    kokura.enter(ticket)
-    refute kyukoudai.exit(ticket)
+    @kokura.enter(ticket)
+    assert @nishikokura.exit(ticket)
+  end
+
+  def test_kokura_to_nishikokura_when_fare_is_not_enough
+    ticket = Ticket.new(170)
+    @kokura.enter(ticket)
+    assert @kyukoudai.exit(ticket)
+  end
+
+  def test_kokura_to_kyukoudai
+    ticket = Ticket.new(210)
+    @kokura.enter(ticket)
+    assert @kyukoudai.exit(ticket)
+  end
+
+  def test_nishikokura_to_kyukoudai
+    ticket = Ticket.new(170)
+    @nishikokura.enter(ticket)
+    assert @kyukoudai.exit(ticket)
   end
 end
